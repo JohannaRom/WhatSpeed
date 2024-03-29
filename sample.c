621ff495332e28de1f9c652c35f8c07359990f4c
JohannaRom <164953939+JohannaRom@users.noreply.github.com>
2024-03-29 08:41:08 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -6,14 +6,6 @@
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
 char cmd[BUFSIZE] = "wc -c < ";
-  if(argc != 2) {
-          printf("Error: Please enter a program to time!\n");
-          return -1;
-     }
-
-     memset(&command, 0, sizeof(command));
-
-     strcat(command, "time ./");
 strcat(cmd, argv[1]);
 system(cmd);
 }
