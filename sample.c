8fc542a678b375cee1cf37d37523df92783f9e74
JohannaRom <164953939+JohannaRom@users.noreply.github.com>
2024-03-29 13:21:52 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,11 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define BUFSIZE 256
-
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-char cmd[BUFSIZE] = "wc -c < ";
-strcat(cmd, argv[1]);
-system(cmd);
+#include <stdio.h> 
+#include<conio.h> 
+#include<process.h> 
+#include<string.h> 
+void main(int argc, char *argv[]) 
+{ 
+FILE *f1; 
+char ch; 
+int c=0,1=0,w=0; 
+if (argc==1) 
+{ printf("invalid parameters"); 
+} if (argc>3) 
+{ 
+printf ("Too many parameters");
+} 
+if (argc==3) 
+{ 
+f1=fopen(argv[2], "r"); //wc2.exe -1 npp.txt 
+if (strcmp(argv[1], "-l")==0) 
+{ 
+while((ch=getc(f1))!=EOF) 
+{ 
+if (ch=='\n') 
+l++; 
+} 
+// l=l-1; 
+printf ("There are %d lines in the file",l); 
+} 
+else 
+if (strcmp(argv[1],"-w")==0) 
+{
+while((ch=getc(f1))!=EOF) 
+{ 
+if (ch=='\n'||ch==' ') 
+w++; 
+} 
+// w=w-1; 
+printf ("There are %d words the file",w); 
+} 
+else 
+if (strcmp(argv[1],"-c")==0) 
+{ 
+while((ch=getc(f1))!=EOF) 
+{ 
+if (ch!='\n'||ch!=' ') 
+c++; 
+} 
+printf ("There are %d characters in the file",c); 
+} 
+else 
+exit(0); 
+} 
+fclose(f1);
 }
+
